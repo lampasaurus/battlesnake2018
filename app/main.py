bf80b9e3172706125e5cf061002a3a2989d132a9
Keiran <keiranreilly7@gmail.com>
2018-03-03 13:30:51 -0800

    stuffs

--- app/main.py
+++ app/main.py
@@ -29,7 +29,7 @@ def start():
     # TODO: Do things with data
 
     return {
-        'color': '#00FF00',
+        'color': '#0000FF',
         'taunt': '{} ({}x{})'.format(game_id, board_width, board_height),
         'head_url': head_url
     }
