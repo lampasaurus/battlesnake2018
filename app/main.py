f464b2378bea475cc83024db5fe8551c6e767a0a
Keiran <keiranreilly7@gmail.com>
2018-03-03 15:25:14 -0800

    asdf

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import bottle
 import os
 import random
-import numpy
+#import numpy
 
 
 @bottle.route('/')
@@ -55,7 +55,6 @@ def createMap(data):
 	for snake in data['snakes']['data']:
 		for coord in snake['body']['data']:
 			map[coord['y']][coord['x']] = WALL
-	print(map);
 	return map
 
 # Expose WSGI app (so gunicorn can find it)
